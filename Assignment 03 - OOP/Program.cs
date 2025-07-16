3e6f5b86412d6f790ec6eae82d4aa4c0035c7d8a
Moa22men <moamenabdelrahman22@gmail.com>
2025-07-16 21:15:40 +0300

    Part 1 solved

--- Assignment 03 - OOP/Program.cs
+++ Assignment 03 - OOP/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Assignment_03___OOP;
+using System;
 
 namespace Assignment_03___OOP
 {
+    #region Part 1
+    /*
+     Question 1: b) To define a blueprint for a class
+     Question 2: a) private
+     Question 3: b) No
+     Question 4: b) Yes, interfaces can inherit from multiple interfaces
+     Question 5: d) implements
+     Question 6: a) Yes
+     Question 7: b) No, all members are implicitly public
+     Question 8: a) To hide the interface members from outside access
+     Question 9: b) No, interfaces cannot have constructors
+     Question 10: c) By separating interface names with commas
+     */
+    #endregion
+
+    #region Part 2
+    
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
